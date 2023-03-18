191e95ccb513db0d6aac33ad74bbdc28025c2aad
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-03-18 06:31:45 +0200

    assignments 001-008

---
+++ assignments/001-008/03.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main()
+{
+  // 01
+  std::cout << "Iam Learning Programming"
+               "\n"
+               "With"
+               "\n"
+               "Elzero"
+               "\n"
+               "Web"
+               "\n"
+               "School"
+               "\n"
+               "<3";
+
+  /* 02
+    std::cout << "Iam Learning Programming"
+              << "\n"
+              << "With"
+              << "\n"
+              << "Elzero"
+              << "\n"
+              << "Web"
+              << "\n"
+              << "School"
+              << "\n"
+                 "<3";
+  */
+
+  return 0;
+}
+
+/*
+Iam Learning Programming
+With
+Elzero
+Web
+School
+<3
+*/
