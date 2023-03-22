1272fe92a605bf0c308226afa0df9308132acc49
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-03-22 11:32:01 +0200

    assignments 024-029

---
+++ assignments/024-029/05.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  // Assign The Values
+  char a = char(100); // 200
+  char b = char(20);  // 20
+  char c = char(45);  // 40
+
+  // Do Not Edit
+  cout << b * c + a << "\n"; // 1000
+}
